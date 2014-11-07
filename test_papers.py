4e082aea9453ed96df2da9ba1931447d83b80d61
jhmann <jhmann@users.noreply.github.com>
2014-11-06 23:30:19 -0500

    First few test files

--- test_papers.py
+++ test_papers.py
@@ -21,6 +21,114 @@ def test_basic():
     assert decide("test_quarantine.json", "watchlist.json", "countries.json") == ["Quarantine"]
 
 
+def test_quarantine():
+    """
+    Inputs a watchlist JSON file, a countries JSON file, and a JSON file with entry records of
+    travellers who should all be quarantined. Included cases: whether they are in transit,
+    visiting or from KAN, whether or not their visa is valid, and whether or not they are on a watchlist.
+    """
+    assert decide("test_JSON_files/01-qyiytransitvyvvyhnwy.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/02-qyiytransitvyvvyhnwn.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/03-qyiytransitvyvvnhnwy.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/04-qyiytransitvyvvnhnwn.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    """assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Quarantine"]
+
+
+def test_secondary():
+    """"""
+    Inputs a watchlist JSON file, a countries JSON file, and a JSON file with entry records of
+    travellers who should all be "Secondary"; i.e. who are on the watchlist.
+    Included cases: whether they are in transit or visiting with valid or invalid visas, or
+    in transit or visiting from a country with no visa needed.
+    """"""
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Secondary"]
+
+
+def test_accept():
+    """"""
+    Inputs a watchlist JSON file, a countries JSON file, and a JSON file with entry records of
+    travellers who should all be accepted.
+    Included cases: whether they are in transit or visiting with visas needed and valid,
+    in transit or visiting with no visa needed, or from KAN.
+    """"""
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Accept"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Accept"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Accept"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Accept"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Accept"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Accept"]
+
+
+def test_reject():
+    """"""
+    Inputs a watchlist JSON file, a countries JSON file, and a JSON file with entry records of
+    travellers who should all be accepted.
+    Included cases: incomplete entry records whether they are in transit,
+    visiting or from KAN, whether or not their visa is valid, and whether or not they are on a watchlist.
+    Also includes rejection for complete entry records where visa is required but invalid or not present.
+    """"""
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+
+    assert decide(test_JSON_files/"xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+    assert decide("test_JSON_files/xxx.json", "watchlist.json", "countries.json") == ["Reject"]
+"""
+
 def test_files():
     with pytest.raises(FileNotFoundError):
         decide("test_returning_citizen.json", "", "countries.json")
